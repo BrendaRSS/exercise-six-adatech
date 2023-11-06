6a59d29864535a26beb5d939362c8702633dda45
Brenda Rayane <brenda.rayane.0018@gmail.com>
2023-11-06 15:09:38 -0300

    feat: style added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function calculateAverage(){
+    let divAverage = document.querySelector(".containerAverage");
+    divAverage.classList.remove("none");
     let average = ((1*2) + (2*15) + (3*18) + (4*25) + (5*40))/100
     let pAverage = document.querySelector(".average")
     pAverage.innerHTML=average;
-    console.logO("A média do restaurante é:", average);
+    console.log("A média do restaurante é:", average);
 }
